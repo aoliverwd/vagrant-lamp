46c801e2853443d526a5af4cd8c0465cc1773501
Alex Oliver <alexoliverwd@gmail.com>
2020-09-27 21:00:17 +0100

    init

---
+++ install.sh
@@ -0,0 +1,25 @@
+sudo apt update
+sudo apt install -y git
+sudo apt install -y curl
+
+sudo apt install -y apache2
+
+sudo a2enmod expires
+sudo a2enmod headers
+sudo a2enmod include
+sudo a2enmod rewrite
+
+sudo apt install -y php 
+sudo apt install -y libapache2-mod-php
+
+sudo service apache2 restart
+
+apt install php-common
+apt install php-all-dev
+
+apt install php-mcrypt
+apt install php-zip
+
+apt install php-mysql
+
+sudo apt install -y mysql-server
